bbf0a3bcb7ee5b4064a24c90039e79c39b33cfad
InmaTc <inmatc94@gmail.com>
2022-03-02 21:06:38 +0100

    Realizada primera migración del proyecto

---
+++ migrations/Version20220302200407.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220302200407 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE acceso (id INT AUTO_INCREMENT NOT NULL, cinta_accede_id INT DEFAULT NULL, supervisada_por_id INT DEFAULT NULL, socio_accede_id INT DEFAULT NULL, hora_entrada DATE NOT NULL COMMENT \'(DC2Type:date_immutable)\', hora_salida DATE DEFAULT NULL COMMENT \'(DC2Type:date_immutable)\', peso INT DEFAULT NULL, INDEX IDX_1268771BA25694FC (cinta_accede_id), INDEX IDX_1268771B71B45C6 (supervisada_por_id), INDEX IDX_1268771B991BE76F (socio_accede_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE cinta (id INT AUTO_INCREMENT NOT NULL, numero INT NOT NULL, grupo_cintas INT NOT NULL, disponible TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE cinta_socio (cinta_id INT NOT NULL, socio_id INT NOT NULL, INDEX IDX_1D75F25415ED566 (cinta_id), INDEX IDX_1D75F254DA04E6A9 (socio_id), PRIMARY KEY(cinta_id, socio_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE empleado (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(255) NOT NULL, apellidos VARCHAR(255) NOT NULL, dni VARCHAR(255) NOT NULL, telefono INT NOT NULL, email VARCHAR(255) NOT NULL, es_supervisor TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE socio (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(255) NOT NULL, apellidos VARCHAR(255) NOT NULL, dni VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE acceso ADD CONSTRAINT FK_1268771BA25694FC FOREIGN KEY (cinta_accede_id) REFERENCES cinta (id)');
+        $this->addSql('ALTER TABLE acceso ADD CONSTRAINT FK_1268771B71B45C6 FOREIGN KEY (supervisada_por_id) REFERENCES empleado (id)');
+        $this->addSql('ALTER TABLE acceso ADD CONSTRAINT FK_1268771B991BE76F FOREIGN KEY (socio_accede_id) REFERENCES socio (id)');
+        $this->addSql('ALTER TABLE cinta_socio ADD CONSTRAINT FK_1D75F25415ED566 FOREIGN KEY (cinta_id) REFERENCES cinta (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE cinta_socio ADD CONSTRAINT FK_1D75F254DA04E6A9 FOREIGN KEY (socio_id) REFERENCES socio (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE acceso DROP FOREIGN KEY FK_1268771BA25694FC');
+        $this->addSql('ALTER TABLE cinta_socio DROP FOREIGN KEY FK_1D75F25415ED566');
+        $this->addSql('ALTER TABLE acceso DROP FOREIGN KEY FK_1268771B71B45C6');
+        $this->addSql('ALTER TABLE acceso DROP FOREIGN KEY FK_1268771B991BE76F');
+        $this->addSql('ALTER TABLE cinta_socio DROP FOREIGN KEY FK_1D75F254DA04E6A9');
+        $this->addSql('DROP TABLE acceso');
+        $this->addSql('DROP TABLE cinta');
+        $this->addSql('DROP TABLE cinta_socio');
+        $this->addSql('DROP TABLE empleado');
+        $this->addSql('DROP TABLE socio');
+    }
+}
