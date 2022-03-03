62dacd63ed8665082deb0ffe2055d5049a243411
InmaTc <inmatc94@gmail.com>
2022-03-03 14:08:43 +0100

    Agregados métodos de búsqueda

--- src/Repository/AccesoRepository.php
+++ src/Repository/AccesoRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Acceso;
+use App\Entity\Socio;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -12,4 +13,14 @@ class AccesoRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Acceso::class);
     }
+
+    public function findSocioAccesos(int $id) : array
+    {
+        return $this
+            ->getEntityManager()
+            ->createQuery("SELECT a FROM  App\\Entity\\Acceso a WHERE a.socioAccede = :socioAccede ORDER BY a.horaSalida ASC")
+            ->setParameter('socioAccede', $id )
+            ->getResult();
+    }
+
 }
--- src/Repository/SocioRepository.php
+++ src/Repository/SocioRepository.php
@@ -6,10 +6,12 @@ use App\Entity\Socio;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
+
 class SocioRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Socio::class);
     }
+
 }
