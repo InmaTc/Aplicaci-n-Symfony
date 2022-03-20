07bda4fab73716dd9eac92a34acea10798285530
InmaTc <inmatc94@gmail.com>
2022-03-20 16:56:31 +0100

    Dejar en el estado anterior.

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
